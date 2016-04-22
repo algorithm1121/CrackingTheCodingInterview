7bcd5fc06344adbc42246285e97311e22907e568
Peter <weijianxing@yunwii.com>
2016-04-22 23:51:20 +0800

    git first commit

---
+++ src/main/java/com/erin/ChpaterOne.java
@@ -0,0 +1,24 @@
+package com.erin;
+
+/**
+ * @author Peter on 2016-04-22.
+ */
+public class ChpaterOne {
+
+    /***
+     * 1.1 Implement an algorithm to determine if a string has all unique characters.
+     * <p>
+     * What if you cannot use additional data structure?
+     */
+    public boolean isUniqueCharacter(String str) {
+        boolean[] char_set = new boolean[256];
+        for (int i = 0; i < str.length(); i++) {
+
+            int val = str.charAt(i);
+
+            if (char_set[val]) return false;
+            else char_set[val] = true;
+        }
+        return true;
+    }
+}
